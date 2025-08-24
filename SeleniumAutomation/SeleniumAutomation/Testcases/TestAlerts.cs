7edf53582b70dceb97dd234d70ff14baf7b168f5
CR3XX12 <crojas8@my.centennialcollege.ca>
2025-08-24 14:43:33 -0400

    feat(selenium-alerts): add demo for handling simple alerts
    - Created 'TestAlerts' class that:
      * Opens https://mail.rediff.com/cgi-bin/login.cgi in Chrome.
      * Clicks the login button that triggers a browser alert.
      * Reads and prints the alert text, pauses 2 s, then accepts the alert.

---
+++ SeleniumAutomation/SeleniumAutomation/Testcases/TestAlerts.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomation.Testcases
+{
+    internal class TestAlerts
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://mail.rediff.com/cgi-bin/login.cgi";
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Window.Maximize();
+
+            driver.FindElement(By.XPath("/html/body/div[2]/div[1]/div/div[2]/div[2]/form/button")).Click();
+
+            IAlert alert= driver.SwitchTo().Alert();
+            Console.WriteLine(alert.Text);
+            Thread.Sleep(2000);
+            alert.Accept();
+        
+        }
+    }
+}
