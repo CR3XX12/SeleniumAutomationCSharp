8a3fa9a52cfda1d5f4aef8e21e77799d9c0c2ea5
CR3XX12 <crojas8@my.centennialcollege.ca>
2025-08-24 20:09:31 -0400

    feat(selenium-TestTryIT): add demo for interacting with iframes
    - Created 'TestTryIT' class that:
      * Navigates to W3Schools “Tryit” page for a form submit example.
      * Switches to the iframe ('iframeResult') and clicks the submit button.
      * Switches back to the default content.
      * Enumerates all '<iframe>' elements on the page and prints their total count plus each frame's 'id' attribute.

---
+++ SeleniumAutomation/SeleniumAutomation/Testcases/TestTryIT.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomation.Testcases
+{
+    internal class TestTryIT
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_form_submit";
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Window.Maximize();
+
+            driver.SwitchTo().Frame("iframeResult");
+            driver.FindElement(By.XPath("//*[@id=\"myForm\"]/input[3]")).Click();
+
+            driver.SwitchTo().DefaultContent();
+
+            var frames = driver.FindElements(By.TagName("iframe"));
+            Console.WriteLine("Total frames are: " + frames.Count);
+
+            foreach (var frame in frames)
+            {
+                Console.WriteLine(frame.GetAttribute("id"));
+            }
+
+           
+
+
+
+            
+        }
+    }
+}
