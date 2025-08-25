469b58fcd8d3862cd56ca9d886e2eb4dca45d698
CR3XX12 <crojas8@my.centennialcollege.ca>
2025-08-25 19:54:48 -0400

    feat(selenium-mouse): demo mouse hover and submenu click
    - Added 'TestMouseMovement' class that:
      * Navigates to Way2Automation’s home page.
      * Locates the “Membership” menu item and performs a mouse‑hover via 'Actions.MoveToElement()'.
      * After the dropdown appears, clicks the “Lifetime Membership” link.
      * Uses an implicit wait and maximizes the window for consistent layout.

---
+++ SeleniumAutomation/SeleniumAutomation/Testcases/TestMouseMovement.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomation.Testcases
+{
+    internal class TestMouseMovement
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://www.way2automation.com/";
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Window.Maximize();
+
+            IWebElement menu = driver.FindElement(By.XPath("//*[@id=\"menu-item-27580\"]/a/span[2]"));
+            Actions action = new Actions(driver);
+            action.MoveToElement(menu).Perform();
+
+            driver.FindElement(By.LinkText("Lifetime Membership")).Click();
+        }
+    }
+}
