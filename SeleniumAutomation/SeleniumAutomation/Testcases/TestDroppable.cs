c3143f6557d731b5683efadfb3111295d8992822
CR3XX12 <crojas8@my.centennialcollege.ca>
2025-08-25 20:51:44 -0400

    feat(selenium-droppable): add demo for dragging a draggable to a droppable target
    - Created 'TestDroppable' class that:
      * Opens https://jqueryui.com/resources/demos/droppable/default.html.
      * Locates the draggable ('#draggable') and the droppable area ('#droppable').
      * Uses 'Actions.DragAndDrop()' to move the element into the target.
      * (Optional) also shows how to drop using an offset if needed.

---
+++ SeleniumAutomation/SeleniumAutomation/Testcases/TestDroppable.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomation.Testcases
+{
+    internal class TestDroppable
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://jqueryui.com/resources/demos/droppable/default.html";
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Window.Maximize();
+
+            IWebElement draggable = driver.FindElement(By.Id("draggable"));
+            IWebElement droppable = driver.FindElement(By.Id("droppable"));
+
+            new Actions(driver).DragAndDrop(draggable, droppable).Perform();
+            //new Actions(driver).DragAndDropToOffset(draggable, 400, 400).Perform(); // Dropping somewhere in the page
+
+        }
+    }
+}
