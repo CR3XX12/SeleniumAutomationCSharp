2bf3b558636537b00b4f635617b129ab64cb498a
CR3XX12 <crojas8@my.centennialcollege.ca>
2025-08-25 20:34:30 -0400

    feat(selenium-resizable): demo resizing a jQuery UI element
    - Added 'TestResizable' class that:
      * Visits https://jqueryui.com/resources/demos/resizable/default.html.
      * Locates the resize handle via XPath.
      * Uses 'Actions.DragAndDropToOffset()' to enlarge the element by 800 px horizontally and 400 px vertically.

---
+++ SeleniumAutomation/SeleniumAutomation/Testcases/TestResizable.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomation.Testcases
+{
+    internal class TestResizable
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://jqueryui.com/resources/demos/resizable/default.html";
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Window.Maximize();
+
+            IWebElement resizable =  driver.FindElement(By.XPath("//*[@id=\"resizable\"]/div[3]"));
+
+            new Actions(driver).DragAndDropToOffset(resizable, 800, 400).Perform();
+        }
+    }
+}
