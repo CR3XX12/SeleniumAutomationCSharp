9ccc3722bb480d6ceba0aa5627bd93067ca2958d
CR3XX12 <crojas8@my.centennialcollege.ca>
2025-08-25 20:13:59 -0400

    feat(selenium-slider): demo drag‑and‑drop on a jQuery UI slider
    - Created 'TestSlider' class that:
      * Opens https://jqueryui.com/resources/demos/slider/default.html
      * Locates the slider handle via XPath
      * Uses 'Actions.DragAndDropToOffset()' to move the slider 400 px to the right

---
+++ SeleniumAutomation/SeleniumAutomation/Testcases/TestSlider.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomation.Testcases
+{
+    internal class TestSlider
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://jqueryui.com/resources/demos/slider/default.html";
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Window.Maximize();
+
+            IWebElement slider = driver.FindElement(By.XPath("//*[@id=\"slider\"]/span"));
+
+            Actions action = new Actions(driver);
+            action.DragAndDropToOffset(slider, 400, 0).Perform();
+        }
+
+    }
+}
