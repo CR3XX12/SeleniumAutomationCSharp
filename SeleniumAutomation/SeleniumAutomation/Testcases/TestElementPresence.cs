f1c1f21db3de35ab1dc88a3f2e84fc636f9dce7a
CR3XX12 <crojas8@my.centennialcollege.ca>
2025-08-27 21:02:17 -0400

    feat(selenium-utility): add generic element‑presence helper
    - Implemented `TestElementPresence` class containing `isElementPresent(By by)`:
      * Uses `driver.FindElement` inside a try/catch to return `true` if the element exists and `false` otherwise.

---
+++ SeleniumAutomation/SeleniumAutomation/Testcases/TestElementPresence.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomation.Testcases
+{
+    internal class TestElementPresence
+    {
+        static IWebDriver driver = new ChromeDriver();
+        static bool isElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            } catch(Exception ex) { return false; }
+        }
+        static void Main(string[] args)
+        {
+            driver = new ChromeDriver();
+            driver.Url = "https://gmail.com";
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.Manage().Window.Maximize();
+
+            Console.WriteLine(isElementPresent(By.XPath("//*[@id=\"identifierId\"]"))); 
+        }
+    }
+}
