b4fd9504193c1d558c367b2c9e313cfc3a34d3f8
CR3XX12 <crojas8@my.centennialcollege.ca>
2025-08-27 20:42:31 -0400

    refactor(testtryit): add screenshot utilities and path helper
    - Introduced 'TestPath' class that prints the project root (two levels up from the current directory) and demonstrates timestamped file naming.
    - In `TestTryIT`:
      * Created `CaptureScreenshot()` – takes a full‑page screenshot and saves it in'\<projectRoot>\screenshot\' with a timestamped “yyyy‑MM‑dd_HH-mm-ss.jpg” name.
      * Created 'CaptureElementScreenshot(IWebElement element)' – does the same for a specific element, prefixing the file name with "Element_".
      * Updated 'Main' to call 'CaptureElementScreenshot' on the submit button, add a red border around it with JS for visual aid, and finally capture a full‑page screenshot.
      * Built the screenshot path via 'Directory.GetParent(Environment.CurrentDirectory).Parent.Parent' to locate the project root regardless of where the test binary runs.
    - Minor cleanup: removed a duplicate ChromeDriver instantiation.

---
+++ SeleniumAutomation/SeleniumAutomation/Testcases/TestPath.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomation.Testcases
+{
+    internal class TestPath
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent);
+
+            DateTime currentTime = DateTime.Now;
+            Console.WriteLine(currentTime.ToString("yyyy-MM-dd_HH-mm-ss")+".jpg");
+        }
+    }
+}
--- SeleniumAutomation/SeleniumAutomation/Testcases/TestTryIT.cs
+++ SeleniumAutomation/SeleniumAutomation/Testcases/TestTryIT.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +11,30 @@ namespace SeleniumAutomation.Testcases
 {
     internal class TestTryIT
     {
+        static IWebDriver driver;
+
+        static void CaptureScreenshot()
+        {
+            DateTime currentTime = DateTime.Now;
+            string filename = currentTime.ToString("yyyy-MM-dd_HH-mm-ss") + ".jpg";
+
+            Screenshot screenshot = driver.TakeScreenshot();
+            screenshot.SaveAsFile(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent + "\\screenshot\\"+filename) ;
+            
+        }
+
+        static void CaptureElementScreenshot(IWebElement element)
+        {
+            DateTime currentTime = DateTime.Now;
+            string filename = "Element_"+currentTime.ToString("yyyy-MM-dd_HH-mm-ss") + ".jpg";
+
+            Screenshot screenshot = ((ITakesScreenshot)element).GetScreenshot();
+            screenshot.SaveAsFile(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent + "\\screenshot\\" + filename);
+
+        }
         static void Main(string[] args)
         {
-            IWebDriver driver = new ChromeDriver();
+            driver = new ChromeDriver();
             driver.Url = "https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_form_submit";
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
             driver.Manage().Window.Maximize();
@@ -21,7 +43,9 @@ namespace SeleniumAutomation.Testcases
             //driver.FindElement(By.XPath("//*[@id=\"myForm\"]/input[3]")).Click();            
 
             IWebElement elem = driver.FindElement(By.XPath("//*[@id=\"myForm\"]/input[3]"));
+            CaptureElementScreenshot(elem);
             ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].style.border='3px solid red'", elem);
+            
 
             Thread.Sleep(3000); // Visual purpose
 
@@ -37,11 +61,10 @@ namespace SeleniumAutomation.Testcases
                 Console.WriteLine(frame.GetAttribute("id"));
             }
 
-           
-
-
-
             
+
+            CaptureScreenshot();
+                                   
         }
     }
 }
