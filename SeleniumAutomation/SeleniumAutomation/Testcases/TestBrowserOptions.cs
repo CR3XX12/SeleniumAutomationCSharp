8dd0fddbe7a3511aef921db279c5e94481db69c2
CR3XX12 <crojas8@my.centennialcollege.ca>
2025-08-27 21:19:27 -0400

    feat(selenium-options): demo custom ChromeOptions
    - Added 'TestBrowserOptions' class that:
      * Builds a 'ChromeOptions' instance with:
        + '--ignore-certificate-errors' to skip SSL warnings.
        + Excluding the 'enable-automation' argument to hide the banner.
        + Disabling Chrome notifications via 'profile.default_content_setting_values.notifications = 2'.
      * Launches Chrome with those options and navigates to https://expired.badssl.com/.
      * Prints the page title to verify that navigation succeeded despite the expired cert.

---
+++ SeleniumAutomation/SeleniumAutomation/Testcases/TestBrowserOptions.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomation.Testcases
+{
+    internal class TestBrowserOptions
+    {
+        static void Main(string[] args)
+        {
+            ChromeOptions options = new ChromeOptions();
+            //options.AddArgument("--headless");
+            options.AddArgument("--ignore-certificate-errors");
+            options.AddExcludedArgument("enable-automation");
+            options.AddUserProfilePreference("profile.default_content_setting_values.notifications",2); // 1 allow 2 block
+            IWebDriver driver = new ChromeDriver(options);
+            driver.Url = "https://expired.badssl.com/";
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Window.Maximize();
+
+            Console.WriteLine(driver.Title);
+
+            //driver.Quit();
+        }
+    }
+}
