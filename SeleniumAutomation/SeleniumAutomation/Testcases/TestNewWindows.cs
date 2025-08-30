b790729527d6b75f8b4b52e40b4b421fb5eaa734
CR3XX12 <crojas8@my.centennialcollege.ca>
2025-08-30 13:07:38 -0400

    feat(selenium-windows): demo opening new tabs and windows
    - Added 'TestNewWindows' class that:
      * Launches Way2Automation landing page.
      * Opens a new tab -> navigates to Gmail, prints the title.
      * Opens a new window -> navigates to Google, prints the title.
      * Finally outputs the current number of window handles.

---
+++ SeleniumAutomation/SeleniumAutomation/Testcases/TestNewWindows.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomation.Testcases
+{
+    internal class TestNewWindows
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://www.way2automation.com";
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Window.Maximize();
+
+            driver.SwitchTo().NewWindow(WindowType.Tab);
+            driver.Url = "https://gmail.com/";
+            Console.WriteLine(driver.Title);
+
+            driver.SwitchTo().NewWindow(WindowType.Window);
+            driver.Url = "https://www.google.com/";
+            Console.WriteLine(driver.Title);
+
+            Console.WriteLine(driver.WindowHandles.Count()); 
+        }
+    }
+}
