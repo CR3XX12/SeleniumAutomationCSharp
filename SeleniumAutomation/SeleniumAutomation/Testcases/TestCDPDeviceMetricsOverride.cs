2dac12ebfae3c913c7cc6839294a4265033b8e7c
CR3XX12 <crojas8@my.centennialcollege.ca>
2025-08-31 16:21:40 -0400

    feat(cdp-device-metrics): demo screen‑size and DPI emulation with CDP
    - Added 'TestCDPDeviceMetricsOverride' class that:
      * Connects to Chrome via 'IDevTools' and accesses the 'Emulation' domain.
      * Calls `SetDeviceMetricsOverride` to pretend the browser is a 375×667 device (iPhone X‑like size) with a 50× device scale factor and mobile mode enabled.
      * Navigates to https://selenium.dev to illustrate the viewport change.

---
+++ SeleniumAutomation/SeleniumAutomation/Testcases/TestCDPDeviceMetricsOverride.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomation.Testcases
+{
+    internal class TestCDPDeviceMetricsOverride
+    {
+        static void Main(string[] args)
+        {
+
+            IWebDriver driver = new ChromeDriver();
+
+            DevToolsSession session = ((IDevTools)driver).GetDevToolsSession();
+            var domains = session.GetVersionSpecificDomains<OpenQA.Selenium.DevTools.V139.DevToolsSessionDomains>();
+            
+            domains.Emulation.SetDeviceMetricsOverride(new OpenQA.Selenium.DevTools.V139.Emulation.SetDeviceMetricsOverrideCommandSettings()
+                {
+                Width = 375,
+                Height = 667,
+                DeviceScaleFactor = 50,
+                Mobile = true,
+            });
+
+            driver.Url = "https://selenium.dev";
+        }
+    }
+}
