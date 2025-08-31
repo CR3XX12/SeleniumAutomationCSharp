2dfc7451f94b6e1479a5e455ecfeecad7d18f79f
CR3XX12 <crojas8@my.centennialcollege.ca>
2025-08-31 16:36:43 -0400

    feat(cdp-network): block image traffic and disable cache with CDP
    - Implemented 'TestCDPBlockNetworkRequest' that:
      * Connects to Chrome via 'IDevTools' and enables the 'Network' domain.
      * Calls 'SetBlockedURLs' to silence all image requests (*.jpg, *.png, *.gif`, *jpeg).
      * Disables HTTP caching with 'SetCacheDisabled'.
      * Opens https://www.expedia.ca/ to demonstrate the blocked requests effect.

---
+++ SeleniumAutomation/SeleniumAutomation/Testcases/TestCDPBlockNetworkRequest.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomation.Testcases
+{
+    internal class TestCDPBlockNetworkRequest
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            DevToolsSession session = ((IDevTools)driver).GetDevToolsSession();
+            var domains = session.GetVersionSpecificDomains<OpenQA.Selenium.DevTools.V139.DevToolsSessionDomains>();
+
+            domains.Network.Enable(new OpenQA.Selenium.DevTools.V139.Network.EnableCommandSettings());
+
+            domains.Network.SetBlockedURLs(new OpenQA.Selenium.DevTools.V139.Network.SetBlockedURLsCommandSettings()
+            {
+                Urls = new string[] { "*.jpg", "*.png", "*.gif", "*jpeg" }
+
+            });
+
+            domains.Network.SetCacheDisabled(new OpenQA.Selenium.DevTools.V139.Network.SetCacheDisabledCommandSettings()
+            {
+                CacheDisabled = true
+            });
+            driver.Url = "https://www.expedia.ca/";
+        }
+    }
+}
