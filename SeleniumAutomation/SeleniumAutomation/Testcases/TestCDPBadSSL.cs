c2727744d2ee5b72dfefddd9f30895338ca3fa11
CR3XX12 <crojas8@my.centennialcollege.ca>
2025-08-31 14:43:31 -0400

    feat(cdp-badssl): demonstrate CDP to silence SSL warnings
    - Added 'TestCDPBadSSL' class that:
      * Establishes a DevTools session via 'IDevTools'
      * Enables the 'Security' domain and calls 'SetIgnoreCertificateErrors' to bypass the expired‑SSL check.
      * Navigates to https://expired.badssl.com/ and confirms the page loads silently

---
+++ SeleniumAutomation/SeleniumAutomation/Testcases/TestCDPBadSSL.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomation.Testcases
+{
+    internal class TestCDPBadSSL
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();           
+
+            DevToolsSession session = ((IDevTools)driver).GetDevToolsSession();
+            var  domains = session.GetVersionSpecificDomains <OpenQA.Selenium.DevTools.V139.DevToolsSessionDomains>();
+            domains.Security.Enable();
+
+            domains.Security.SetIgnoreCertificateErrors(new OpenQA.Selenium.DevTools.V139.Security.SetIgnoreCertificateErrorsCommandSettings()
+                {
+                Ignore = true
+            });
+            driver.Url = "https://expired.badssl.com/";
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Window.Maximize();
+        }
+    }
+}
