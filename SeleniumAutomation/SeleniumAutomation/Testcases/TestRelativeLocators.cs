f6e59ea71808e553cf7de9adb9900b79a5636610
CR3XX12 <crojas8@my.centennialcollege.ca>
2025-08-31 13:06:42 -0400

    feat(selenium-relative): add demo for Selenium RelativeLocators
    - Created 'TestRelativeLocators' class showcasing the new
      Selenium 4 relative locator feature:
      * On the Way2Automation jQuery demo page it finds:
        + The email input that is *above* a <select> element.
        + The city input that is *below* a <select>.
        + A password field that is to the *right of* its label.
      * Sends sample data into each of those elements.

---
+++ SeleniumAutomation/SeleniumAutomation/Testcases/TestRelativeLocators.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomation.Testcases
+{
+    internal class TestRelativeLocators
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://www.way2automation.com/way2auto_jquery/index.php";
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Window.Maximize();
+
+            IWebElement above =  driver.FindElement(RelativeBy.WithLocator(By.TagName("input")).Above(By.TagName("Select")));
+            above.SendKeys("trainer@way2automation.com");
+
+            IWebElement below = driver.FindElement(RelativeBy.WithLocator(By.TagName("input")).Below(By.TagName("Select")));
+            below.SendKeys("New Delhi");
+
+            IWebElement rightOf = driver.FindElement(RelativeBy.WithLocator(By.XPath("//*[@id=\"load_form\"]/fieldset[7]/input")).RightOf(By.TagName("label")));
+            rightOf.SendKeys("Password123");
+        }
+    }
+}
