133158efe0db88864cb50c55a5e86b88de45beeb
CR3XX12 <crojas8@my.centennialcollege.ca>
2025-08-31 17:07:21 -0400

    feat(cdp-basic-auth): bypass HTTP Basic Auth via Chrome DevTools Protocol
    - Added 'TestCDPBasicAuth' that:
      * Enables the 'Network' domain in a CDP session.
      * Encodes the 'admin:admin' credentials to Base64.
      * Sets an 'Authorization' header for all subsequent requests using 'SetExtraHTTPHeaders'.
      * Navigates to https://the-internet.herokuapp.com/basic_auth (including the credentials in the URL for reference) to prove the header was applied

---
+++ SeleniumAutomation/SeleniumAutomation/Testcases/TestCDPBasicAuth.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools;
+using OpenQA.Selenium.DevTools.V139.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomation.Testcases
+{
+    internal class TestCDPBasicAuth
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            DevToolsSession session = ((IDevTools)driver).GetDevToolsSession();
+            var domains = session.GetVersionSpecificDomains<OpenQA.Selenium.DevTools.V139.DevToolsSessionDomains>();
+
+            domains.Network.Enable(new OpenQA.Selenium.DevTools.V139.Network.EnableCommandSettings());
+
+            var encodedAuth = Convert.ToBase64String(Encoding.Default.GetBytes("admin:admin"));
+
+            var headerSettings = new SetExtraHTTPHeadersCommandSettings
+            {
+                Headers = new Headers()
+                {
+                    {"authorization", "Basic " + encodedAuth}
+                }
+            };
+
+            domains.Network.SetExtraHTTPHeaders(headerSettings);
+
+            driver.Url = "https://admin:admin@the-internet.herokuapp.com/basic_auth"; 
+
+        }
+    }
+}
