463afcd0f9782bc2c009d63cf77b4517de224982
CR3XX12 <crojas8@my.centennialcollege.ca>
2025-08-31 15:02:33 -0400

    feat(cdp-geolocation): demo overriding browser location with CDP
    - Implemented 'TestCDPGeoLocationOverride' class that:
      * Connects to Chrome via 'IDevTools' and accesses the 'Emulation' domain.
      * Calls 'SetGeolocationOverride' to fake the browser’s GPS coordinates (Latitude = 37.5326, Longitude = 127.0246, Accuracy = 100 m).
      * Loads https://www.gps-coordinates.net/my-location to show the fake location in action.

---
+++ SeleniumAutomation/SeleniumAutomation/Testcases/TestCDPGeoLocationOverride.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomation.Testcases
+{
+    internal class TestCDPGeoLocationOverride
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            DevToolsSession session = ((IDevTools)driver).GetDevToolsSession();
+            var domains = session.GetVersionSpecificDomains<OpenQA.Selenium.DevTools.V139.DevToolsSessionDomains>();
+
+            domains.Emulation.SetGeolocationOverride(new OpenQA.Selenium.DevTools.V139.Emulation.SetGeolocationOverrideCommandSettings()
+            {
+                Latitude = 37.532600, 
+                Longitude = 127.024612,
+                Accuracy = 100
+            });
+            driver.Url = "https://www.gps-coordinates.net/my-location";
+        }
+    }
+}
