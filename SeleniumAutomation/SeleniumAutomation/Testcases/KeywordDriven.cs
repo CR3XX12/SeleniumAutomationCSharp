61ea7ec18e223032b36ddaa28d60702b1b82e5a2
CR3XX12 <crojas8@my.centennialcollege.ca>
2025-09-02 00:21:39 -0400

    feat(keyword‑driven): add basic keyword engine with XML locators
    - Implemented 'KeywordDriven' class that:
      * Exposes 'Click()' and 'Type()' actions which read locators from 'XMLLocatorReader.GetLocatorValue(page, element, type)'.
      * Supports ID, XPATH and CSS locator types.
      * Integrates with Microsoft.Extensions.Configuration to read the browser type, target URL and implicit‑wait from 'appsettings.json'.
      * Instantiates the appropriate WebDriver (Chrome/Firefox) based on config.
      * Shows a simple demo: logging into the “LoginPage” by using the keyword methods instead of hard‑coded 'FindElement' calls.

---
+++ SeleniumAutomation/SeleniumAutomation/Testcases/KeywordDriven.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using SeleniumAutomation.utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomation.Testcases
+{
+    internal class KeywordDriven
+    {
+        static IWebDriver driver;
+
+        public static void Click(string pageName, string locatorName, string locatorType)
+        {
+            if (locatorType.Contains("ID"))
+            {
+                driver.FindElement(By.Id(XMLLocatorReader.GetLocatorValue(pageName, locatorName, locatorType))).Click();
+
+            }
+            else if (locatorType.Contains("XPATH"))
+            {
+                driver.FindElement(By.XPath(XMLLocatorReader.GetLocatorValue(pageName, locatorName, locatorType))).Click();
+            }
+            else if (locatorType.Contains("CSS"))
+            {
+                driver.FindElement(By.CssSelector(XMLLocatorReader.GetLocatorValue(pageName, locatorName, locatorType))).Click();
+            }        
+
+        }
+        public static void Type(string pageName, string locatorName, string locatorType, string value)
+        {
+            if (locatorType.Contains("ID"))
+            {
+                driver.FindElement(By.Id(XMLLocatorReader.GetLocatorValue(pageName, locatorName, locatorType))).SendKeys("trainer@way2automation.com");
+
+            }
+            else if (locatorType.Contains("XPATH"))
+            {
+                driver.FindElement(By.XPath(XMLLocatorReader.GetLocatorValue(pageName, locatorName, locatorType))).SendKeys("trainer@way2automation.com");
+            }
+            else if (locatorType.Contains("CSS"))
+            {
+                driver.FindElement(By.CssSelector(XMLLocatorReader.GetLocatorValue(pageName, locatorName, locatorType))).SendKeys("trainer@way2automation.com");
+            }
+        }
+            static void Main(string[] args)
+            {
+
+                IConfiguration configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "\\resources\\")
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .Build();
+
+                if (configuration["Appsettings:browser"].Equals("chrome"))
+                {
+                    driver = new ChromeDriver();
+                }
+                else if (configuration["Appsettings:browser"].Equals("firefox"))
+                {
+                    driver = new FirefoxDriver();
+                }
+
+                driver.Url = configuration["Appsettings:testsiteurl"];
+                driver.Manage().Window.Maximize();
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(int.Parse(configuration["AppSettings:implicit.wait"]));
+
+
+                //string browser = configuration["Appsettings:browser"];
+
+                //driver.FindElement(By.Id(XMLLocatorReader.GetLocatorValue("LoginPage","username","ID"))).SendKeys("trainer@way2automation.com");
+                //driver.FindElement(By.XPath(XMLLocatorReader.GetLocatorValue("LoginPage", "password", "XPATH"))).SendKeys("fsegsg");
+
+                Type("LoginPage", "username", "ID", "trainer@way2automation.com");
+                Type("LoginPage", "password", "XPATH", "sfdsfdsfs");
+
+            }
+        }
+    }
+
