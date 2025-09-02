85f3ac4e34450b6addbab032fd73561b18088533
CR3XX12 <crojas8@my.centennialcollege.ca>
2025-09-01 23:08:38 -0400

    feat(xml-locators): add reader for UI‑locator XML store
    - Created 'XMLLocatorReader' utility that:
      * Loads `resources/locators.xml` and parses it with 'XmlDocument'.
      * Builds an XPath expression to fetch a '<LocatorValue>' node based on
        '<pageName>', '<elementName>', and the desired '<LocatorType>'.
      * Returns the actual locator string (e.g., “username”, “//*[@id='pass']”).
      * Includes a quick demo ('Main') that prints a couple of locator values.

---
+++ SeleniumAutomation/SeleniumAutomation/utilities/XMLLocatorReader.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace SeleniumAutomation.utilities
+{
+    internal class XMLLocatorReader
+    {
+        public static string GetLocatorValue(string pageName, string elementName, string locatorType)
+        {
+            string locatorValue = null;
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "\\resources\\locators.xml");
+
+            //Load the XML File
+            XmlElement root = xmlDoc.DocumentElement;
+
+            //Construct XPATH expression to select the specified element under the specified page with given locator type
+
+            string xpath = $"/locators/{pageName}/{elementName}[LocatorType='{locatorType}']/LocatorValue";
+
+            //Select the locator value node
+            XmlNode locatorValueNode = root.SelectSingleNode(xpath);
+
+            if (locatorValueNode != null) {
+                locatorValue = locatorValueNode.InnerText;
+            }
+            return locatorValue;
+                }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetLocatorValue("LoginPage", "username", "ID"));
+
+            Console.WriteLine(GetLocatorValue("RegistrationPage", "password", "XPATH"));
+        }
+
+    }
+}
