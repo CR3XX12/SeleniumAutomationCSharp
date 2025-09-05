21d3af559f2ab9deb8f1371a3e2e691d130bd957
CR3XX12 <crojas8@my.centennialcollege.ca>
2025-09-05 19:46:45 -0400

    feat(logging): add log4net configuration and simple logger demo
    - Created 'log4net.config' that
      * Configures a root logger with level "ALL" and two appenders:
        1. 'ConsoleAppender' for immediate CLI output.
        2. 'RollingFileAppender' that writes to 'Logs\logfile.log', rolls on file size (10 MB) and keeps up to 10 backups.
      * Uses a unified pattern layout for all messages.
    
    - Implemented 'TestLogger' class that:
      * Loads the configuration via `XmlConfigurator.Configure`.
      * Logs one message at each level (Info, Debug, Error, Warn, Fatal).

---
+++ SeleniumAutomation/SeleniumAutomation/Testcases/TestLogger.cs
@@ -0,0 +1,27 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomation.Testcases
+{
+    internal class TestLogger
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(TestLogger));
+
+        static void Main(string[] args)
+        {
+            var logRepository = LogManager.GetRepository(System.Reflection.Assembly.GetExecutingAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("resources\\log4net.config"));
+
+            log.Info("Text execution started");
+            log.Debug("This is a debug message");
+            log.Error("This is an Error");
+            log.Warn("This is a warning");
+            log.Fatal("This is a fatal message");
+        }
+    }
+}
