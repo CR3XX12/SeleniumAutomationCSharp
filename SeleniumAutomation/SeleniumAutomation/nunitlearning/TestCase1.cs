460156e1dd4146616f95eac51aeaf4295d67887c
CR3XX12 <crojas8@my.centennialcollege.ca>
2025-09-07 14:00:45 -0400

    feat(testing): add NUnit test skeleton & package references
    - Included a minimal 'TestCase1' class with two demo tests ('DoLogin' and 'DoUserReg') using NUnit '[Test]' attributes.
    - Updated the project’s '<ItemGroup>' with all required NuGet packages:
      * log4net, Microsoft.Extensions.Configuration.Json,
      * Microsoft.NET.Test.Sdk, MySql.Data,
      * NUnit, NUnit3TestAdapter,
      * Selenium.Support, Selenium.WebDriver,
      * SeleniumExtras.WaitHelpers.
    - These dependencies support the Selenium‑based test framework, configuration handling, logging, database access, and the NUnit test runner.

---
+++ SeleniumAutomation/SeleniumAutomation/nunitlearning/TestCase1.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomation.nunitlearning
+{
+    internal class TestCase1
+    {
+        [Test]
+        public void DoLogin()
+        {
+            Console.WriteLine("Executing Login Test");
+        }
+
+        [Test]
+        public void DoUserReg()
+        {
+            Console.WriteLine("Executing User reg Test");
+        }
+    }
+}
