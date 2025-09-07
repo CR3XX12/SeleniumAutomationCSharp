962e370000109b55be20840a679820add62f5bd7
CR3XX12 <crojas8@my.centennialcollege.ca>
2025-09-07 13:41:41 -0400

    feat(db‑utility): add MySQL connection manager and sample query
    - Implemented 'DBManager':
      * Stores a 'MySqlConnection' instance and opens it via
        'SetMySQLDBConnection()' using a local connection string.
      * Provides 'GetQuery(string query)' that returns a list of string results
        for any SELECT statement.
      * Wraps calls in try/catch blocks and writes any error messages to the console.
    - Added 'TestDB' demo that:
      * Calls 'SetMySQLDBConnection()'.
      * Queries the 'selenium' database for the author of a tutorial and prints the first result

---
+++ SeleniumAutomation/SeleniumAutomation/Testcases/TestDB.cs
@@ -0,0 +1,19 @@
+﻿using SeleniumAutomation.utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomation.Testcases
+{
+    internal class TestDB
+    {
+        static void Main(string[] args)
+        {
+            DBManager.SetMySQLDBConnection();
+            Console.WriteLine(DBManager.GetQuery("select tutorial_author from selenium where tutorial_id = 1")[0]);
+
+        }
+    }
+}
