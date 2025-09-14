fa0c5b004dde8a9eceb9cc86dfcf2184b0fd4fea
CR3XX12 <crojas8@my.centennialcollege.ca>
2025-09-14 16:13:55 -0400

    feat(parallel-selenium): add WebDriver setup/teardown to parallel test suite
    - Updated 'TestParallel' to:
      * Create a 'ChromeDriver' in the '[SetUp]' method and navigate to https://www.facebook.com/.
      * Safely quit, dispose, and null‑out the driver in '[TearDown]' (prevents shared state across parallel tests).
      * 'LoginTest' now interacts with the page: sends the username and password, waits a bit, and logs the action with a timestamp

--- SeleniumAutomation/SeleniumAutomation/nunitlearning/TestParallel.cs
+++ SeleniumAutomation/SeleniumAutomation/nunitlearning/TestParallel.cs
@@ -1,7 +1,10 @@
 ﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,26 @@ namespace SeleniumAutomation.nunitlearning
     [Parallelizable]
     internal class TestParallel
     {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+           driver = new ChromeDriver();
+           driver.Url = "https://www.facebook.com/";            
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if(driver!= null)
+            {
+                driver.Quit();
+                driver.Dispose();
+                driver = null;
+            }
+        }
+
         [Parallelizable(ParallelScope.Children)]
         [Test, TestCaseSource("GetTestData")]
         public void LoginTest(string username, string password)
@@ -17,7 +40,9 @@ namespace SeleniumAutomation.nunitlearning
             DateTime currentTime = DateTime.Now;
             string time = currentTime.ToString("yyyy-MM_dd_HH-mm-ss");
             Console.WriteLine($"Logging in with {username} using {password} ---- {time}");
-            Thread.Sleep(1000);
+            driver.FindElement(By.Id("email")).SendKeys(username);
+            driver.FindElement(By.Id("pass")).SendKeys(password);
+            Thread.Sleep(5000);
         }
 
         public static IEnumerable<TestCaseData> GetTestData()
@@ -26,21 +51,21 @@ namespace SeleniumAutomation.nunitlearning
             return DataUtil.GetTestDataFromExcel(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "\\resources\\testdata.xlsx", "LoginTest", columns);
         }
 
-        [Parallelizable(ParallelScope.Children)]
-        [Test, TestCaseSource("GetUserRegTestData")]
-        public void UserRegTest(string firstname, string lastname)
-        {
-            DateTime currentTime = DateTime.Now;
-            string time = currentTime.ToString("yyyy-MM_dd_HH-mm-ss");
+        //[Parallelizable(ParallelScope.Children)]
+        //[Test, TestCaseSource("GetUserRegTestData")]
+        //public void UserRegTest(string firstname, string lastname)
+        //{
+        //    DateTime currentTime = DateTime.Now;
+        //    string time = currentTime.ToString("yyyy-MM_dd_HH-mm-ss");
 
-            Console.WriteLine(firstname + " ----" + lastname+"--"+time);
-            Thread.Sleep(1000);
-        }
+        //    Console.WriteLine(firstname + " ----" + lastname+"--"+time);
+        //    Thread.Sleep(1000);
+        //}
 
-        public static IEnumerable<TestCaseData> GetUserRegTestData()
-        {
-            var columns = new List<string> { "firstname", "lastname" };
-            return DataUtil.GetTestDataFromExcel(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "\\resources\\testdata.xlsx", "UserRegTest", columns);
-        }
+        //public static IEnumerable<TestCaseData> GetUserRegTestData()
+        //{
+        //    var columns = new List<string> { "firstname", "lastname" };
+        //    return DataUtil.GetTestDataFromExcel(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "\\resources\\testdata.xlsx", "UserRegTest", columns);
+        //}
     }
 }
