9926672cbe89c0adb0df8641c824d57d8c4d83af
CR3XX12 <crojas8@my.centennialcollege.ca>
2025-09-13 22:22:37 -0400

    add(testcase): show NUnit retry logic
    - Implemented 'RetryTests.TestMethod_withretries':
      * Uses '[Test, Retry(3)]' to automatically retry failed executions up to 3 times.
      * Increments a counter on each run and deliberately fails until the third attempt.
      * On the third attempt, a PASS message is emitted.
      * Logs each attempt via 'TestContext.Progress'.

---
+++ SeleniumAutomation/SeleniumAutomation/nunitlearning/RetryTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomation.nunitlearning
+{
+    [TestFixture]
+    internal class RetryTests
+    {
+        private int _counter = 0;
+
+        [Test, Retry(3)]
+        public void TestMethod_withretries()
+        {
+            _counter++;
+
+            if(_counter < 3)
+            {
+                TestContext.Progress.WriteLine($"Test attemp {_counter}");
+                Assert.Fail("Simulated failure");
+            }
+            
+            // Pass on the 3rd attempt
+            Assert.Pass("Test passed on attempt " + _counter);
+
+
+        }
+
+    }
+}
