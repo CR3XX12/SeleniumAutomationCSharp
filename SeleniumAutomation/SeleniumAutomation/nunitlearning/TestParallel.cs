681dad7dcca886307afd160d46c61609dee93c7c
CR3XX12 <crojas8@my.centennialcollege.ca>
2025-09-14 15:56:18 -0400

    feat(parallelism): add data‑driven NUnit tests that run in parallel
    - Created 'TestParallel':
      * Uses '[Parallelizable]' on the class and '[ParallelScope.Children]' on each test method.
      * Two data‑driven tests ('LoginTest' and 'UserRegTest') each read rows from 'resources\testdata.xlsx' via 'DataUtil.GetTestDataFromExcel'
      * Prints the executed username/credentials and a timestamp, then sleeps 1 s to simulate work and to test the parallel is working

---
+++ SeleniumAutomation/SeleniumAutomation/nunitlearning/TestParallel.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomation.nunitlearning
+{
+    [Parallelizable]
+    internal class TestParallel
+    {
+        [Parallelizable(ParallelScope.Children)]
+        [Test, TestCaseSource("GetTestData")]
+        public void LoginTest(string username, string password)
+        {
+            DateTime currentTime = DateTime.Now;
+            string time = currentTime.ToString("yyyy-MM_dd_HH-mm-ss");
+            Console.WriteLine($"Logging in with {username} using {password} ---- {time}");
+            Thread.Sleep(1000);
+        }
+
+        public static IEnumerable<TestCaseData> GetTestData()
+        {
+            var columns = new List<string> { "username", "password" };
+            return DataUtil.GetTestDataFromExcel(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "\\resources\\testdata.xlsx", "LoginTest", columns);
+        }
+
+        [Parallelizable(ParallelScope.Children)]
+        [Test, TestCaseSource("GetUserRegTestData")]
+        public void UserRegTest(string firstname, string lastname)
+        {
+            DateTime currentTime = DateTime.Now;
+            string time = currentTime.ToString("yyyy-MM_dd_HH-mm-ss");
+
+            Console.WriteLine(firstname + " ----" + lastname+"--"+time);
+            Thread.Sleep(1000);
+        }
+
+        public static IEnumerable<TestCaseData> GetUserRegTestData()
+        {
+            var columns = new List<string> { "firstname", "lastname" };
+            return DataUtil.GetTestDataFromExcel(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "\\resources\\testdata.xlsx", "UserRegTest", columns);
+        }
+    }
+}
