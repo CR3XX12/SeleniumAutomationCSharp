0f0b24a26610bd12fd19e278e0de03c3e50590ea
CR3XX12 <crojas8@my.centennialcollege.ca>
2025-09-13 23:28:23 -0400

    feat(testing): add NUnit setup/teardown fixture and test parameterization
    - Implemented 'BaseTest' marked with '[SetUpFixture]':
      * Provides '[OneTimeSetUp]' and '[OneTimeTearDown]' hooks that run once before/after the entire test suite, simulating database connection creation and closure with console messages.
    
    - Added 'TestParameterization' class that demonstrates:
      * '[TestCase]' attributes for simple hard‑coded data sets.
      * '[TestCaseSource]' with a 'GetTestData' iterator that yields
        'TestCaseData' objects for first/last names.
      * Uses 'yield return' to lazily produce test data, ideal for larger data sets.

---
+++ SeleniumAutomation/SeleniumAutomation/nunitlearning/BaseTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomation.nunitlearning
+{
+    [SetUpFixture]
+    internal class BaseTest
+    {
+        [OneTimeSetUp]
+        public void CreateDbConn()
+        {
+            TestContext.Progress.WriteLine($"Creating DB Connection"); ;
+        }
+
+        [OneTimeTearDown]
+        public void CloseDbConn()
+        {
+            TestContext.Progress.WriteLine("Closing DB Connection");
+        }
+    }
+}
---
+++ SeleniumAutomation/SeleniumAutomation/nunitlearning/TestParameterization.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomation.nunitlearning
+{
+    internal class TestParameterization
+    {
+
+        [Test]
+        [TestCase("trainer@way2automation.com", "sfdsfdsfd")]
+        [TestCase("info@way2automation.com", "xfdsfdawdsfd")]
+        [TestCase("java@way2automation.com", "pfdsfdsfdwad")]
+        public void LoginTest(string username, string password)
+        {
+            Console.WriteLine($"Logging in with {username} using {password}");
+        }
+
+        [Test, TestCaseSource("GetTestData")]
+        public void UserRegTest(string firstname, string lastname)
+        {
+            Console.WriteLine(firstname+" ----"+lastname);
+        }
+
+        public static IEnumerable<TestCaseData> GetTestData()
+        {
+            //Yield will wait until all returrns are successful completed
+            //The purpose of using yield is to provide a simple way to implement an iterator method
+            //and temporarily suspend the execution until the next value is requested.
+            //This allows the method to resume execution from where it left off when the next value is requested,
+
+            yield return new TestCaseData("John", "Cena");
+            yield return new TestCaseData("Chris", "Rojas");
+            yield return new TestCaseData("John", "Wick");
+
+        }
+    }
+}
