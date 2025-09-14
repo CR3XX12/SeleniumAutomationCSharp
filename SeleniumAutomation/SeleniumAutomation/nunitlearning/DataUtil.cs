adb21e40ecfdc00b9bc3c7caebf2fec67aeb6cc6
CR3XX12 <crojas8@my.centennialcollege.ca>
2025-09-14 01:20:04 -0400

    feat(excel‑data): add EPPlus helper & data‑driven tests
    - Implemented 'DataUtil.GetTestDataFromExcel':
      * Initializes EPPlus for non‑commercial use.
      * Reads specified columns from a given worksheet, validating the sheet and column names.
      * Returns each row as 'TestCaseData', allowing easy consumption by NUnit 'TestCaseSource'.
    
    - Created 'TestParameterizationExcel' class that:
      * Uses 'GetTestData()' and 'GetUserRegTestData()' to fetch login and registration data from 'resources\testdata.xlsx'.
      * Passes each row to its respective test method ('LoginTest', 'UserRegTest') by NUnit’s 'TestCaseSource'.
    
    - Added a real 'testdata.xlsx' file in the 'resources' folder containing the expected columns
      ('username', 'password', 'firstname', 'lastname') for the example tests.

---
+++ SeleniumAutomation/SeleniumAutomation/nunitlearning/DataUtil.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomation.nunitlearning
+{
+    class DataUtil
+    {
+        static DataUtil()
+        {
+            // Set the license correctly for EPPlus v8+
+            ExcelPackage.License.SetNonCommercialPersonal("Christopher");            
+        }
+
+        public static IEnumerable<TestCaseData> GetTestDataFromExcel(string filePath, string sheetName, List<string> columnNames)
+        {
+            //ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+            var testData = new List<TestCaseData>();
+
+            // Load Excel file
+            using (var package = new ExcelPackage(new FileInfo(filePath)))
+            {
+                var worksheet = package.Workbook.Worksheets[sheetName];
+                if (worksheet == null)
+                {
+                    throw new ArgumentException($"Sheet '{sheetName}' not found in the Excel file.");
+                }
+
+                // Find column indices for specified column names
+                var columnIndices = new Dictionary<string, int>();
+                for (int col = 1; col <= worksheet.Dimension.End.Column; col++)
+                {
+                    var cellValue = worksheet.Cells[1, col].Value?.ToString();
+                    if (columnNames.Contains(cellValue))
+                    {
+                        columnIndices[cellValue] = col;
+                    }
+                }
+
+                // Check if all column names are found
+                foreach (var columnName in columnNames)
+                {
+                    if (!columnIndices.ContainsKey(columnName))
+                    {
+                        throw new ArgumentException($"Column name '{columnName}' not found in the Excel file.");
+                    }
+                }
+
+                // Read test data
+                for (int row = 2; row <= worksheet.Dimension.End.Row; row++)
+                {
+                    var data = new List<string>();
+                    foreach (var columnName in columnNames)
+                    {
+                        data.Add(worksheet.Cells[row, columnIndices[columnName]].Value?.ToString());
+                    }
+                    testData.Add(new TestCaseData(data.ToArray()));
+                }
+            }
+
+            return testData;
+        }
+    }
+}
---
+++ SeleniumAutomation/SeleniumAutomation/nunitlearning/TestParameterizationExcel.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomation.nunitlearning
+{
+    internal class TestParameterizationExcel
+    {
+        [Test,TestCaseSource("GetTestData")]
+        public void LoginTest(string username, string password)
+        {
+            Console.WriteLine($"Logging in with {username} using {password}");
+        }
+
+        public static IEnumerable<TestCaseData> GetTestData()
+        {            
+            var columns = new List<string> { "username", "password" };
+            return DataUtil.GetTestDataFromExcel(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "\\resources\\testdata.xlsx", "LoginTest", columns);
+        }
+
+        [Test, TestCaseSource("GetUserRegTestData")]
+        public void UserRegTest(string firstname, string lastname)
+        {
+            Console.WriteLine(firstname + " ----" + lastname);
+        }
+
+        public static IEnumerable<TestCaseData> GetUserRegTestData()
+        {           
+            var columns = new List<string> { "firstname", "lastname" };
+            return DataUtil.GetTestDataFromExcel(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "\\resources\\testdata.xlsx", "UserRegTest", columns);
+        }
+    }
+}
